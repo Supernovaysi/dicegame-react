c71cdd1fa364e369789eb6247473ae3473b087e5
ysi <books13456@gmail.com>
2021-10-19 00:05:11 +0900

    Simplify App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-import Board from "./Board";
+import { useState } from 'react';
+import Board from './Board';
+import Button from './Button';
 
+function random(n) {
+  return Math.ceil(Math.random() * n);
+}
 
 function App() {
+  const [myHistory, setGameHistory] = useState([]);
+  const [otherHistory, setOtherGameHistory] = useState([]);
+
+  const handleRollClick = () => {
+    const nextMyNum = random(6);
+    const nextOtherNum = random(6);
+    setGameHistory([...myHistory, nextMyNum]);
+    setOtherGameHistory([...otherHistory, nextOtherNum]);
+  };
+
+  const handleClearClick = () => {
+    setGameHistory([]);
+    setOtherGameHistory([]);
+  };
+
   return (
     <div>
       <Button onClick={handleRollClick}>던지기</Button>
       <Button onClick={handleClearClick}>처음부터</Button>
       <div>
-        <Board name="나" color="blue" num={num} sum={sum} gameHistory={gameHistory} />
-        <Board name="상대" color="red" num={otherNum} sum={otherSum} gameHistory={otherGameHistory} />
+        <Board name="나" color="blue" gameHistory={myHistory} />
+        <Board name="상대" color="red" gameHistory={otherHistory} />
       </div>
     </div>
   );
--- src/Board.js
+++ src/Board.js
@@ -1,41 +1,19 @@
-import { useState } from 'react';
 import Dice from './Dice';
-import Button from './Button';
 
-function random(n) {
-  return Math.ceil(Math.random() * n);
-}
-
-function Board({name, color}) {
-  const [num, setNum] = useState(1);
-  const [sum, setSum] = useState(0);
-  const [gameHistory, setGameHistory] = useState([]);
-
-  const handleRollClick = () => {
-    const nextNum = random(6);
-    setNum(nextNum);
-    setSum(sum + nextNum);
-    setGameHistory(gameHistory.concat([nextNum]));
-  };
-
-  const handleClearClick = () => {
-    setNum(1);
-    setSum(0);
-    setGameHistory([]);
-  };
-
+function Board({ name, color, gameHistory }) {
+  const num = gameHistory[gameHistory.length - 1] || 1;
+  const sum = gameHistory.reduce((a, b) => a + b, 0);
+  
   return (
     <div>
-      <div>
-        <h2>{name}</h2>
-        <Dice color={color} num={num} />
-        <h2>총점</h2>
-        <p>{sum}</p>
-        <h2>기록</h2>
-        {gameHistory.join(', ')}
-      </div>
+      <h1>{name}</h1>
+      <Dice color={color} num={num} />
+      <h2>총점</h2>
+      <p>{sum}</p>
+      <h2>기록</h2>
+      <p>{gameHistory.join(', ')}</p>
     </div>
-  ); 
+  );
 }
 
 export default Board;
